3c023f078ac9c6439a181973b953cbc39228a437
TCJ319 <torresc1423@gmail.com>
2024-08-29 21:23:37 -0600

    pagina con js y html para sumar dos numeros de manera muy sensilla

---
+++ Tarea3/app.js
@@ -0,0 +1,8 @@
+function sum(a,b){
+    return a+b
+}
+
+let a = parseInt(prompt("escribe un numero"));
+let b = parseInt(prompt("escribe otro numero"));
+let resultado = sum(a,b);
+document.write(resultado)
